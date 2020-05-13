91d71cb1b56ee745e4d10621273304b4b99187f3
Michaelkulinich <kulinich@chapman.edu>
2020-05-12 18:55:28 -0700

    Final

--- sendEmail.py
+++ sendEmail.py
@@ -6,26 +6,16 @@ import webScrap
 # Selenium to automate through web
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
-import requests
-from bs4 import BeautifulSoup
-#
-# f = open("practiceDataBase.txt", "r")
-# count = 0
-# for line in f:
-#     if count == 0:
-#         email = line.strip()
-#         count += 1
-#     elif count == 1:
-#         state = line.strip()
-#         count += 1
-#     else:
-#         text = line.strip()
-#
-# f.close()
+
+
+
 state = input("Input your state: ").capitalize()
 email = input("Enter your email: ")
 email_body_string = webScrap.getData(state)
 
+# Selenium step by step to send an email through gmail
+# First it logs in then clicks all the necessary buttons and writes in the
+# email information
 driver = webdriver.Chrome()
 driver.set_page_load_timeout(15)
 driver.maximize_window()
@@ -35,19 +25,16 @@ driver.get("https://accounts.google.com/signin/v2/identifier?service=mail&passiv
 driver.find_element_by_id('identifierId').send_keys("CVTFCPSC353@gmail.com")
 driver.find_element_by_id('identifierId').send_keys(Keys.ENTER)
 
-time.sleep(5)
+time.sleep(7)
 driver.find_element_by_name("password").send_keys("Cvtf1231$")
 driver.find_element_by_name("password").send_keys(Keys.ENTER)
-time.sleep(5)
-#driver.find_element_by_class_name("T-I J-J5-Ji T-I-KE L3 T-I-JW")
+time.sleep(7)
 driver.find_element_by_xpath("//div[@class='T-I J-J5-Ji T-I-KE L3']").click()
 time.sleep(4)
 driver.find_element_by_xpath("//textarea[@name='to']").send_keys(email)
-time.sleep(1)
+time.sleep(2)
 driver.find_element_by_xpath("//input[@name='subjectbox']").send_keys("CoronaVirus Update")
 driver.find_element_by_xpath("//div[@class='Am Al editable LW-avf tS-tW']").send_keys(email_body_string)
-# Am Al editable LW-avf tS-tW
 driver.find_element_by_xpath("//div[@class='T-I J-J5-Ji aoO v7 T-I-atl L3']").click()
-#T-I J-J5-Ji aoO v7 T-I-atl L3
 time.sleep(5)
 driver.quit()
--- webScrap.py
+++ webScrap.py
@@ -12,27 +12,24 @@ import requests
 from bs4 import BeautifulSoup
 
 def getData(state):
-	print("Hi")
-
 	page = requests.get("https://www.worldometers.info/coronavirus/country/us/")
 	soup = BeautifulSoup(page.content, 'lxml')
+	# Locate the table that holds all the info about the us
 	table = soup.find('table')
 	table_rows = table.find_all('tr')
 	table1 = []
-	#state = input("Input your state: ").capitalize()
-	# state = "Hawaii"
+
 	for tr in table_rows:
 		td = tr.find_all('td')
 		row = [i.text.strip() for i in td]
 		table1.append(row)
+	# Grab all the useful information fro that state
 	for i in range(len(table1)):
 		for j in range(len(table1[i])):
 			if table1[i][j] == state:
 				my_string = "Hello friends,\n\nI hope that you and your family are staying healthy during these uncertain times.\n    Below are the latest statistics about COVID-19 in {}:\n    The total number of cases right now is: {}\n    The total number of deaths is: {}\n    The number of active cases is: {}\n\nPlease visit cdc.gov to follow their guidelines and to stay up to date about the CoronaVirus.\n\nHave a safe and healthy time,\nCVTF".format(state, table1[i][j+1],table1[i][j+3],table1[i][j+5])
+				 #When I tried making this line shorter by breaking up the string
+				 #it was giving me problemns with selenium
 
 
 				return my_string
-
-
-# d = getData("Hawaii")
-# print(d)
